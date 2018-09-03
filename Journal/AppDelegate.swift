bfc7c413ce9a92848e4887b208fbe0ea58f328d7
Andras Hollo <hollo.andris@gmail.com>
2018-09-03 01:52:14 -0700

    Initial Commit

---
+++ Journal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Journal
+//
+//  Created by Andras Hollo on 2018. 09. 03..
+//  Copyright © 2018. Andras Hollo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Journal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Andras Hollo on 2018. 09. 03..
+//  Copyright © 2018. Andras Hollo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
